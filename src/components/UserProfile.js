0dbdfb8ea92d5eb432cbf159d528286d3b015dab
Tatiana Perez <tjperez@ncsu.edu>
2021-04-17 22:45:46 -0400

    name, bio, location rendering

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import User from './UserData.js'
+import lodash from 'lodash'
 
 
-    
 
 
 function UserProfile() {
@@ -12,17 +12,24 @@ function UserProfile() {
 
     return (
         <div>
-            {users.map((user) => (
-            <h1>{user.username}</h1>
+            {users.map((user) => {
+
+                return <>
+                    <div>{user.prof_pic}</div>
+                    <h1>{user.username}</h1>
+
+                       <div>{user.location_info.country}</div>
+                       <div>{user.bio_text}</div>
+                       <h2 class="font-bold">{user.username}'s Feed:</h2>
+                       
+                       </>
+
+                    
+})}   
 
 
-
-            ))}   
-                
-            
-        
-        
         </div>
+
     )
 }
 
