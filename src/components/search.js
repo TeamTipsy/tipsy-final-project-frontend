5c7205620d80f05ac1492ebefc565a63537b6938
Tatiana Perez <tjperez@ncsu.edu>
2021-04-19 00:00:39 -0400

    a basic form w/o request to API

--- src/components/search.js
+++ src/components/search.js
@@ -3,7 +3,16 @@ import { useState, useEffect } from 'react'
 
 
 function Search() {
-    return (null)
-}
+    const [search, setSearch] = useState('')
+    const handleChange = (event) => {
+      setSearch(event.target.value)
+    }
+    return (
+      <>
+        <input type="text" value={search} onChange={handleChange} className="border-black border-2 m-3" />
+        <button onClick={() =>console.log(search)} className="bg-black hover:bg-gray-500 text-white font-bold py-0.5 px-4">Search</button>
+      </>
+    )
+  }
 
 export default Search 
