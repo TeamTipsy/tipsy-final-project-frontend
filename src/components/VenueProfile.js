1aa9ef82c062b66a7d442cf12a18c64d9f1f97f7
Albany <albany.prince@gmail.com>
2021-04-19 21:25:07 -0400

    tailwind customizations

--- src/components/VenueProfile.js
+++ src/components/VenueProfile.js
@@ -6,17 +6,16 @@ function VenueProfile() {
     const [venues, setVenue] = useState(Venues)
      console.log(venues)
     return (
-        <div>
+        <div className='px-10 m-8'>
             {venues.map((venue) => {
             
             return <>
-            <div className='venue-profile'>
-                <div className='info-card'>
-                    <h1>{venue.venue_name}</h1>
+            <div className='venue-profile bg-brand-beau-blue m-8 px-60 py-40 rounded-r-md rounded-l-md'>
+                <div className='text-black  pl-80'>
+                    <h1 className='text-4xl font-black'>{venue.venue_name}</h1>
                     <h2>{venue.venue_info.street_address}</h2>
-
                     <h2>Followers:{venue.followers.length}</h2>
-                    <h4>{venue.venue_type}</h4>
+                    <h4>{venue.bdw_type}</h4>
                 </div>
                 
             </div>
