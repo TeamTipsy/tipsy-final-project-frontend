55ef098eec1eb88823d4a48d5d56f301dba878b8
Tatiana Perez <tjperez@ncsu.edu>
2021-04-26 23:16:24 -0400

    can select user or venue for search

--- src/components/search.js
+++ src/components/search.js
@@ -13,8 +13,7 @@ import {
 
 function Search() {
     const [search, setSearch] = useState('')
-    let [endpoint, setEndpoint] = useState('')
-    // let endpoint = ''
+    const [endpoint, setEndpoint] = useState('')
     const [searchResults, setSearchResults] = useState([])
 
 
@@ -36,11 +35,11 @@ function Search() {
       setSearch(event.target.value)
     }
 
-    // const handleEndpoint = (event) => {
-    //   setEndpoint(event.target.value)
-    //   console.log(endpoint)
-    // }
-
+    const handleEndpoint = (event) => {
+     
+      setEndpoint(event.target.value)
+      console.log(endpoint)
+    }
 
 
     return (
@@ -48,6 +47,21 @@ function Search() {
 
         <div className="flex h-12 lg:mx-72 sm:mx-12">
         <input type="text" value={search} onChange={handleChange} className="w-full h-12 pl-4 mr-3 border-2 rounded-full border-brand-beau-blue" placeholder="Find a drink" />
+
+
+        <button onClick={() => doSearch(search)} className="px-3 text-sm font-medium leading-4 text-white border border-transparent rounded-lg h-10px bg-brand-red hover:bg-brand-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Search</button>
+       
+        </div>
+        
+        <label for="search" className="h-auto ml-2"></label>
+
+<form name="searchfilter" id="search" className="mr-3 mt-3 h-6 flex">
+  <input onChange={handleEndpoint} type="radio" value="users"  className="mr-1 p-4" checked={endpoint === 'users'}/>
+  <div>{'users'}</div>
+  <input onChange={handleEndpoint} type="radio" value="venues" className="mr-1" checked={endpoint === 'venues'}/>
+  {'venues'}
+</form> 
+  
 
         <button onClick={() => doSearch(search)} type="submit" className="px-3 text-sm font-medium leading-4 text-white border border-transparent rounded-lg h-10px bg-brand-red hover:bg-brand-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Search</button>
 
