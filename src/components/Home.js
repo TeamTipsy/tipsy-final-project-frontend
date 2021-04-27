882d30eee6d39d11446b189f80e7f4341a6139c2
arthurstuart <arthur.stuart10@gmail.com>
2021-04-26 23:12:10 -0400

    Venues on discovery page, but users aren't quite yet.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,21 @@ import { useState, useEffect } from 'react'
 import { Router, Switch } from 'react-router'
 import Cta from './Cta.js'
 import Search from './search.js'
+import TopRatedVenues from './TopRatedVenues.js'
+import TopRatedUsers from "./TopRatedUsers.js";
 
 
 function Home() {
     return ( 
     <>
-    <div className="mt-10 justify-center"> 
+    <div className="justify-center mt-16"> 
         <Search/>
-        <Cta/>
+        <h2 className="text-xl mt-28 ">Top Rated Venues</h2>
+        <TopRatedVenues className="col-span-1"
+        />
+        <h2 className="mt-12 text-xl">Top Rated Users</h2>
+        <TopRatedUsers className="col-span-1"
+        />
     </div>
    
  
