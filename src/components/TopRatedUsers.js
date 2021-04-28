4902c77422edf6853dfc87c58b72fb25f2b947ca
Tatiana Perez <tjperez@ncsu.edu>
2021-04-27 20:51:53 -0400

    user prof error

--- src/components/TopRatedUsers.js
+++ src/components/TopRatedUsers.js
@@ -25,9 +25,17 @@ function TopRatedUsers({ token }) {
         })}, [])
         console.log('top', topUsers)
 
-        if (selectedUser) {
-            return <UserProfile selectedUser={selectedUser} setSelectedUser={setSelectedUser} token={token}/>
+    // if (selectedUser) {
+    //     return <UserProfile selectedUser={selectedUser} setSelectedUser={setSelectedUser} token={token}/>
+    // }
 
+        if (selectedUser) {
+        return <Redirect
+            to={{
+            pathname: "/UserProfile/",
+            state: { selectedUser:selectedUser, setSelectedUser:setSelectedUser, token:token }
+            }}
+        />
         }
 
 
