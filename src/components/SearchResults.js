29b25d55a545c195436167a05036263020a19f22
Tatiana Perez <tjperez@ncsu.edu>
2021-04-28 22:26:39 -0400

    search work?

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -8,50 +8,55 @@ export default function SearchResults(props) {
     const search = props.location.state.search
     const searchResults = props.location.state.searchResults
     const endpoint = props.location.state.endpoint
-    const toggleResults = props.location.state.toggleResults
     console.log(searchResults)
 
+    function ContentRenderer() {
+        if (endpoint === 'venues') {
+            return <div className="mt-5 mx-10 ">
+                        <Search/>
+                            <div className="center text-4xl mt-3 text-brand-dark-blue">Results:</div>
+                            <ul className="divide-y divide-gray-200 text-brand-dark-blue h-28">
+                            {searchResults.map((result)=> (
+                                <li className="py-8 w-full">
+                                    <div className="flex space-x-3">
+                                    <img src={result.prof_pic} className="h-10 w-12 rounded-full"/>
+                                    <div className="text-2xl"><a className="hover:text-brand-red">{result.venue_name}</a><div className="text-sm">{result.venue_type}</div></div>
+                                    <div className="py-4 pl-36">
+                                    <div>{result.phone_num}</div>
+                                    <p>{result.street_address}, {result.city}, {result.state}</p>
+                                    <a className="hover:text-brand-red" href={result.web_url}>{result.web_url}</a>
+                                    </div>
+                                    </div>
+                                </li>
+                            ))}
+                            </ul>
+                        </div>
+        } else if (endpoint === 'users') {
+            return <div className="mt-5 mx-10 ">
+                        <Search/>
+                        <div className="center text-4xl mt-3 text-brand-dark-blue">Results:</div>
+                        <ul className="divide-y divide-gray-200 text-brand-dark-blue h-28">
+                        {searchResults.map((result)=> (
+                            <li className="py-8 w-full">
+                                <div className="flex space-x-3">
+                                <img src={result.prof_pic} className="h-10 w-12 rounded-full"/>
+                                <div className="text-2xl"><a className="hover:text-brand-red">{result.username}</a><div className="text-sm">{result.venue_type}</div></div>
+                                <div className="py-4 pl-36">
+                                
+                                <p>{result.city}, {result.state}</p>
+                                <a className="hover:text-brand-red" href={result.web_url}>{result.web_url}</a>
+                                </div>
+                                </div>
+                            </li>
+                        ))}
+                        </ul>
+                </div>
+        }
+    }
 
     return (
         <>
-        {endpoint === "venues"  ? (
-        <div className="mt-5 mx-10 ">
-        <Search/>
-            <div className="center text-4xl mt-3 text-brand-dark-blue">Results:</div>
-            <ul className="divide-y divide-gray-200 text-brand-dark-blue h-28">
-            {searchResults.map((result)=> (
-                <li className="py-8 w-full">
-                    <div className="flex space-x-3">
-                    <img src={result.prof_pic} className="h-10 w-12 rounded-full"/>
-                    <div className="text-2xl"><a className="hover:text-brand-red">{result.venue_name}</a><div className="text-sm">{result.venue_type}</div></div>
-                    <div className="py-4 pl-36">
-                    <div>{result.phone_num}</div>
-                    <p>{result.street_address}, {result.city}, {result.state}</p>
-                    <a className="hover:text-brand-red" href={result.web_url}>{result.web_url}</a>
-                    </div>
-                    </div>
-                </li>
-            ))}
-            </ul>
-        </div>) : (<div className="mt-5 mx-10 ">
-        <Search/>
-            <div className="center text-4xl mt-3 text-brand-dark-blue">Results:</div>
-            <ul className="divide-y divide-gray-200 text-brand-dark-blue h-28">
-            {searchResults.map((result)=> (
-                <li className="py-8 w-full">
-                    <div className="flex space-x-3">
-                    <img src={result.prof_pic} className="h-10 w-12 rounded-full"/>
-                    <div className="text-2xl"><a className="hover:text-brand-red">{result.username}</a><div className="text-sm">{result.venue_type}</div></div>
-                    <div className="py-4 pl-36">
-                    
-                    <p>{result.city}, {result.state}</p>
-                    <a className="hover:text-brand-red" href={result.web_url}>{result.web_url}</a>
-                    </div>
-                    </div>
-                </li>
-            ))}
-            </ul>
-        </div>)}
+            <ContentRenderer/>
         </>
     );
 }
--- src/components/search.js
+++ src/components/search.js
@@ -15,74 +15,50 @@ function Search() {
     const [search, setSearch] = useState('')
     const [endpoint, setEndpoint] = useState('')
     const [searchResults, setSearchResults] = useState([])
-    const [toggleResults, setToggleResults] = useState(false)
 
-
-    
     const doSearch = (search) => {
       axios.get(
           `https://tipsy-backend.herokuapp.com/${endpoint}/?search=${search}`, 
           {
-              
           }).then((response) => {
-          setToggleResults(true)
           console.log('resp', response)
           setSearchResults([...response.data])
-          
-
       })}
 
-
     const handleChange = (event) => {
-      setToggleResults(false)
       setSearch(event.target.value)
     }
 
     const handleEndpoint = (event) => {
-      setToggleResults(false)
       setEndpoint(event.target.value)
       console.log(endpoint)
     }
 
 
 
-    return (
-      <>
-
-        <div className="flex h-12 lg:mx-72 sm:mx-12">
-        <input type="text" value={search} onChange={handleChange} className="w-full h-12 pl-4 mr-3 border-2 rounded-full border-brand-beau-blue" placeholder="Find a drink" />
-
-
-        <button onClick={() => doSearch(search)} className="px-3 text-sm font-medium leading-4 text-white border border-transparent rounded-lg h-10px bg-brand-red hover:bg-brand-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Search</button>
-        
-        </div>
-        
-        <label for="search" className="h-auto ml-2"></label>
-
-<form name="searchfilter" id="search" className="mr-3 mt-3 h-6 flex">
-  <input onChange={handleEndpoint} type="radio" value="users"  className="mr-1 p-4" checked={endpoint === 'users'}/>
-  <div>{'users'}</div>
-  <input onChange={handleEndpoint} type="radio" value="venues" className="mr-1" checked={endpoint === 'venues'}/>
-  {'venues'}
-</form> 
-  
-
-{searchResults && searchResults.length > 0 ? (
-
-<Redirect
-  to={{
-    pathname: "/SearchResults/",
-    state: { searchResults: searchResults, search:search, endpoint: endpoint, toggleResults: toggleResults  }
-  }}
-/>
-
-
-) : (<div></div>)}
-
-
-
-      </>
-    )
-  }
+  return (
+    <>
+      <div className="flex h-12 lg:mx-72 sm:mx-12">
+      <input type="text" value={search} onChange={handleChange} className="w-full h-12 pl-4 mr-3 border-2 rounded-full border-brand-beau-blue" placeholder="Find a drink" />
+      <button onClick={() => doSearch(search)} className="px-3 text-sm font-medium leading-4 text-white border border-transparent rounded-lg h-10px bg-brand-red hover:bg-brand-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Search</button>
+      </div>
+      <label for="search" className="h-auto ml-2"></label>
+      <form name="searchfilter" id="search" className="mr-3 mt-3 h-6 flex">
+        <input onChange={handleEndpoint} type="radio" value="users"  className="mr-1 p-4" checked={endpoint === 'users'}/>
+        <div>{'users'}</div>
+        <input onChange={handleEndpoint} type="radio" value="venues" className="mr-1" checked={endpoint === 'venues'}/>
+        {'venues'}
+      </form> 
+      {searchResults && searchResults.length > 0 ? (
+        <Redirect
+          to={{
+            pathname: "/SearchResults/",
+            state: { searchResults: searchResults, search:search, endpoint: endpoint }
+          }}
+        />
+      ) : (<div></div>)}
+    </>
+  )
+}
 
 export default Search 
