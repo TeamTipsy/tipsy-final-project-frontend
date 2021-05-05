f3c43dcfe0ad125de158c5950c2c73fcb1ccf2d0
Albany <albany.prince@gmail.com>
2021-05-05 10:32:47 -0400

    like button color

--- src/components/VenueProfile.js
+++ src/components/VenueProfile.js
@@ -158,7 +158,7 @@ function VenueProfile({ selectedVenue, token }) {
                         </div>
                         <div className="flex mt-2 mx-4 justify-end">
                                 <a onClick={() => handleLikeClick(post.post_id)} className="hover:text-brand-dark-blue text-brand-beau-blue inline-block">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="brand-dark-blue">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
                                 </svg>
                                 </a> 
