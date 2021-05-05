fbe3bc76a6967572365bbe4267009237ea642a3d
Albany <albany.prince@gmail.com>
2021-05-04 22:21:55 -0400

    again

--- src/components/VenueProfile.js
+++ src/components/VenueProfile.js
@@ -87,24 +87,6 @@ function VenueProfile({ selectedVenue, token }) {
         })
     }   
     
-    // function openContent(e, venueBlurb) {
-    //     
-    //     var i, tabcontent, tablinks;
-      
-    //     tabcontent = document.getElementsByClassName("tabcontent");
-    //     for (i = 0; i < tabcontent.length; i++) {
-    //       tabcontent[i].style.display = "none";
-    //     }
-      
-    //     tablinks = document.getElementsByClassName("tablinks");
-    //     for (i = 0; i < tablinks.length; i++) {
-    //       tablinks[i].className = tablinks[i].className.replace(" active", "");
-    //     }
-      
-    //     
-    //     document.getElementById(venueBlurb).style.display = "block";
-    //     e.currentTarget.className += " active";
-    //   }
    
     return (
         
